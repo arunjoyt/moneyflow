a2f12a3be97cc3f18ad1b32cb71ae91ccc09a8ea
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-11-08 16:10:30 +0530

    setting user to logged on user

--- moneyflow/money_flow/doctype/medical_data/medical_data.py
+++ moneyflow/money_flow/doctype/medical_data/medical_data.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, Arun Joy Thekkiniyath and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MedicalData(Document):
-	pass
+    def before_save(self):
+		# user should always be logged in user.
+        self.user = frappe.session.user
